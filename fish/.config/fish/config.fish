e2e57d42fd0c23fd4a5d108b8baa668b92c5490c
Monti <ooy.yoo@gmail.com>
2025-03-10 12:40:30 +0800

    feat(fish): tw to launch mcp server middleware

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -88,7 +88,7 @@ alias ta "tmux attach"
 alias tk "tmux kill-session -t" # can follow by session name
 alias tl "tmux list-sessions"
 alias tp "tmux new-session -s pyenv -e PYENV_INIT=1"
-alias tw "tmux new-session -c ~/.z/projects/capybara/www.capybara.run/ \; new-window"
+alias tw "tmux new-session -c ~/.z/projects/capybara/www.capybara.run/ \; new-window 'npx @agentdeskai/browser-tools-server'"
 
 # if type -q exa
 alias ls eza
