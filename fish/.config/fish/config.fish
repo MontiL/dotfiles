31628e68f094a969128333bb7507dd243851c59e
Monti <ooy.yoo@gmail.com>
2025-03-11 09:18:50 +0800

    refact: use pnpm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -89,7 +89,7 @@ alias tk "tmux kill-session -t" # can follow by session name
 alias tl "tmux list-sessions"
 alias tp "tmux new-session -s pyenv -e PYENV_INIT=1"
 alias tw "tmux new-session -c ~/.z/projects/capybara/www.capybara.run/ \; new-window"
-alias tws "tmux new-session -c ~/.z/projects/capybara/www.capybara.run/ \; new-window \; split-window -v 'npx @agentdeskai/browser-tools-server' \; select-pane -U"
+alias tws "tmux new-session -c ~/.z/projects/capybara/www.capybara.run/ \; new-window \; split-window -v 'pnpm dlx @agentdeskai/browser-tools-server' \; select-pane -U"
 
 # if type -q exa
 alias ls eza
