d5158ec4ede7a33a56797dcbb25a551820b459f6
Monti <ooy.yoo@gmail.com>
2025-03-28 12:22:07 +0800

    refact

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -122,6 +122,13 @@ return {
       --[[ use("hrsh7th/cmp-emoji") ]]
       -- Plug 'nvim-lua/completion-nvim'
     },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
     config = function()
       -- reference:
       -- https://github.com/hrsh7th/nvim-cmp#setup
