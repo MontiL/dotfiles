13ea6ca7fb6d697d405b99ebd95458f27c91fef3
Monti <ooy.yoo@gmail.com>
2025-03-28 10:13:00 +0800

    refact(nvim)

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -26,5 +26,4 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true, notify = false },
-  change_detection = { notify = false },
 })
