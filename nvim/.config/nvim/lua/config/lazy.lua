fd1714ae964bc566b9af491074628012d19d4fa3
Monti <ooy.yoo@gmail.com>
2025-03-28 12:15:05 +0800

    feat(nvim): no change_detection

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -26,4 +26,7 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true, notify = false },
+  change_detection = {
+    notify = false,
+  },
 })
