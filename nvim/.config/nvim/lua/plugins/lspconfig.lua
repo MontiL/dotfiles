ac344a549fcfa41e8013fe55b61b99a93a80b68c
Monti <ooy.yoo@gmail.com>
2025-03-28 12:16:57 +0800

    refact

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -11,59 +11,6 @@ return {
     "folke/neodev.nvim",
   },
   config = function()
-    -- Setup LSP handlers
-    local function setup_handlers()
-      -- Diagnostic symbols in the sign column (gutter)
-      local signs = { Error = " ", Warn = " ", Hint = "● ", Info = "" }
-
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
-
-      local config = {
-        -- disable virtual text
-        --[[ virtual_text = true, ]]
-        virtual_text = { prefix = "●" },
-        -- show signs
-        signs = { active = signs },
-        update_in_insert = true,
-        underline = true,
-        severity_sort = true,
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
-      }
-      vim.diagnostic.config(config)
-      vim.lsp.handlers["textDocument/hover"] = function(_, result, _, handler_config)
-        local hover_config = handler_config or {}
-        hover_config.border = "rounded"
-        vim.lsp.util.open_floating_preview(
-          vim.lsp.util.convert_input_to_markdown_lines(result.contents),
-          "markdown",
-          hover_config
-        )
-      end
-      vim.lsp.handlers["textDocument/signatureHelp"] = function(_, result, ctx, handler_config)
-        local sig_config = handler_config or {}
-        sig_config.border = "rounded"
-        local signatures = result.signatures
-        if ctx and ctx.client_id then
-          signatures = vim.lsp.util.stylize_markdown(signatures, { client_id = ctx.client_id })
-        end
-        vim.lsp.util.open_floating_preview(
-          vim.lsp.util.convert_input_to_markdown_lines(signatures),
-          "markdown",
-          sig_config
-        )
-      end
-    end
-
     -- Server Configurations
     -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#dockerls
     local servers = {
@@ -139,9 +86,53 @@ return {
       end,
     })
 
-    -- Setup handlers
-    setup_handlers()
-    -- require("lsp.null-ls")
-    --[[ require("lsp.lsp-installer") ]]
+    -- Diagnostic symbols in the sign column (gutter)
+    local signs = { Error = " ", Warn = " ", Hint = "● ", Info = "" }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+    local config = {
+      -- disable virtual text
+      --[[ virtual_text = true, ]]
+      virtual_text = { prefix = "●" },
+      -- show signs
+      signs = { active = signs },
+      update_in_insert = true,
+      underline = true,
+      severity_sort = true,
+      float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    }
+    vim.diagnostic.config(config)
+    -- Show line diagnostics automatically in hover window
+    vim.lsp.handlers["textDocument/hover"] = function(_, result, _, handler_config)
+      local hover_config = handler_config or {}
+      hover_config.border = "rounded"
+      vim.lsp.util.open_floating_preview(
+        vim.lsp.util.convert_input_to_markdown_lines(result.contents),
+        "markdown",
+        hover_config
+      )
+    end
+    vim.lsp.handlers["textDocument/signatureHelp"] = function(_, result, ctx, handler_config)
+      local sig_config = handler_config or {}
+      sig_config.border = "rounded"
+      local signatures = result.signatures
+      if ctx and ctx.client_id then
+        signatures = vim.lsp.util.stylize_markdown(signatures, { client_id = ctx.client_id })
+      end
+      vim.lsp.util.open_floating_preview(
+        vim.lsp.util.convert_input_to_markdown_lines(signatures),
+        "markdown",
+        sig_config
+      )
+    end
   end,
 }
