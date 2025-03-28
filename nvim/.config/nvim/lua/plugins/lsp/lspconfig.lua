43542808d94dcbd9c322cb23062cf9ddf4d6e6e5
Monti <ooy.yoo@gmail.com>
2025-03-28 12:51:09 +0800

    feat(nvim): mason-tool-installer

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -12,66 +12,67 @@ return {
   config = function()
     -- Server Configurations
     -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#dockerls
-    local servers = {
-      bashls = {},
-      vimls = {},
-      taplo = {},
-      yamlls = {},
-      lua_ls = {
-        Lua = {
-          runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = "LuaJIT",
-          },
-          diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = { "vim" },
-          },
-          workspace = {
-            -- Make the server aware of Neovim runtime files
-            --[[ library = vim.api.nvim_get_runtime_file("", true), ]]
-            checkThirdParty = false,
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = { enable = false },
-        },
-      },
-      -- luau_lsp = {},
-      -- NOTE: need to have `pyrightconfig.json` in root folder
-      -- pyright = {
-      --   Python = {
-      --     analysis = {
-      --       autoSearchPaths = true,
-      --       diagnosticMode = "workspace",
-      --       useLibraryCodeForTypes = true,
-      --     }
-      --   }
-      -- },
-      pylsp = {},
-      -- web
-      ts_ls = {},
-      tailwindcss = {},
-      emmet_ls = {},
-      jsonls = {},
-      prosemd_lsp = {},
-      -- studying
-      --[[ vuels = {}, ]]
-      --[[ astro = {}, ]]
-      --[[ svelte = {}, ]]
-      prismals = {},
-      -- dockerls = {},
-    }
+    -- local servers = {
+    --   bashls = {},
+    --   vimls = {},
+    --   taplo = {},
+    --   yamlls = {},
+    --   lua_ls = {
+    --     Lua = {
+    --       runtime = {
+    --         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+    --         version = "LuaJIT",
+    --       },
+    --       diagnostics = {
+    --         -- Get the language server to recognize the `vim` global
+    --         globals = { "vim" },
+    --       },
+    --       workspace = {
+    --         -- Make the server aware of Neovim runtime files
+    --         --[[ library = vim.api.nvim_get_runtime_file("", true), ]]
+    --         checkThirdParty = false,
+    --       },
+    --       -- Do not send telemetry data containing a randomized but unique identifier
+    --       telemetry = { enable = false },
+    --     },
+    --   },
+    --   -- luau_lsp = {},
+    --   -- NOTE: need to have `pyrightconfig.json` in root folder
+    --   -- pyright = {
+    --   --   Python = {
+    --   --     analysis = {
+    --   --       autoSearchPaths = true,
+    --   --       diagnosticMode = "workspace",
+    --   --       useLibraryCodeForTypes = true,
+    --   --     }
+    --   --   }
+    --   -- },
+    --   pylsp = {},
+    --   -- web
+    --   ts_ls = {},
+    --   tailwindcss = {},
+    --   emmet_ls = {},
+    --   jsonls = {},
+    --   prosemd_lsp = {},
+    --   -- studying
+    --   --[[ vuels = {}, ]]
+    --   --[[ astro = {}, ]]
+    --   --[[ svelte = {}, ]]
+    --   prismals = {},
+    --   -- dockerls = {},
+    -- }
 
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
     -- Setup mason so it can manage external tooling
     require("mason").setup()
 
     -- Ensure the servers above are installed
     local mason_lspconfig = require("mason-lspconfig")
-    mason_lspconfig.setup({ ensure_installed = vim.tbl_keys(servers) })
+    -- mason_lspconfig.setup({ ensure_installed = vim.tbl_keys(servers) })
     local lspconfig = require("lspconfig")
 
     -- Automatic server setup (advanced feature) :h mason-lspconfig-automatic-server-setup
@@ -80,7 +81,7 @@ return {
         lspconfig[server_name].setup({
           capabilities = capabilities,
           -- on_attach = on_attach,
-          settings = servers[server_name],
+          -- settings = servers[server_name],
         })
       end,
     })
